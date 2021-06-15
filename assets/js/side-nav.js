19fa9be8f327b335121694ab83bf1696732d227f
Ruth <ruth@Ruths-MacBook-Pro.local>
2021-06-15 14:38:57 -0500

    fix

--- assets/js/side-nav.js
+++ assets/js/side-nav.js
@@ -2,13 +2,13 @@ jQuery(function ($) {
   // Dropdown menu
   $('.sidebar-dropdown > a').click(function (e) {
     $('.sidebar-submenu').slideUp(200);
-    if ($(this).parent().hasClass('active')) {
-      $('.sidebar-dropdown').removeClass('active');
+    if ($(this).parent().hasClass('active1')) {
+      $('.sidebar-dropdown').removeClass('active1');
       $(this).parent().removeClass('active') && e.stopPropagation();
     } else {
-      $('.sidebar-dropdown').removeClass('active');
+      $('.sidebar-dropdown').removeClass('active1');
       $(this).next('.sidebar-submenu').slideDown(200);
-      $(this).parent().addClass('active') && e.stopPropagation();
+      $(this).parent().addClass('active1') && e.stopPropagation();
     }
   });
 
