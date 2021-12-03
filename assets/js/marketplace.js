d22d5870a49fc927f22770055743b71b775b8bee
Matthew Petrovic <matt.t.petrovic@gmail.com>
2021-12-03 14:16:58 -0500

    First draft of marketplace page

---
+++ assets/js/marketplace.js
@@ -0,0 +1,24 @@
+setTimeout(function () {
+  (function ($) {
+
+    $(document).ready(function () {
+      let descriptions = {};
+      let $this = $(this);
+      
+      $.get({
+        url: '/business-standards-api/grants/business-lifecycle/',
+        dataType: 'json',
+        success: function (data) {
+          console.log(data);
+          for (let k in data) {
+            for (let i = 0, l = data[k].length; i < l; i++) {
+              let identifier = data[k][i]['Identifier'];
+              $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
+            }
+          }
+        }
+      });
+    });
+
+  })(jQuery);
+}, 10);
