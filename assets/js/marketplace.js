bd20379274e1dd4f1272472bfbfb6fef65c00e1c
Matthew Petrovic <matt.t.petrovic@gmail.com>
2021-12-08 14:52:22 -0500

    Requested changes, making view description into modals, reorder first table

--- assets/js/marketplace.js
+++ assets/js/marketplace.js
@@ -1,24 +1,27 @@
-setTimeout(function () {
-  (function ($) {
+let interval = setInterval(function () {
+  if (typeof jQuery != 'undefined') {
+    (function ($) {
 
-    $(document).ready(function () {
-      let descriptions = {};
-      let $this = $(this);
-      
-      $.get({
-        url: '/business-standards-api/grants/business-lifecycle/',
-        dataType: 'json',
-        success: function (data) {
-          console.log(data);
-          for (let k in data) {
-            for (let i = 0, l = data[k].length; i < l; i++) {
-              let identifier = data[k][i]['Identifier'];
-              $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
+      $(document).ready(function () {
+        let descriptions = {};
+        let $this = $(this);
+        
+        $.get({
+          url: '/business-standards-api/grants/business-lifecycle/',
+          dataType: 'json',
+          success: function (data) {
+            console.log(data);
+            for (let k in data) {
+              for (let i = 0, l = data[k].length; i < l; i++) {
+                let identifier = data[k][i]['Identifier'];
+                $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
+              }
             }
           }
-        }
+        });
       });
-    });
 
-  })(jQuery);
-}, 250);
+    })(jQuery);
+    clearInterval(interval);
+  }
+}, 100);
