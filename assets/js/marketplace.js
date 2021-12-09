895a59904862a2285b8375a97d2dd1e799b97ab6
Matthew Petrovic <matt.t.petrovic@gmail.com>
2021-12-09 13:08:25 -0500

    Resolving data issues and known things

--- assets/js/marketplace.js
+++ assets/js/marketplace.js
@@ -1,29 +1,21 @@
-(function  () {
-  let interval = setInterval(function () {
-    if (typeof jQuery != 'undefined') {
-      (function ($) {
+(function ($) {
+  $(document).ready(function () {
+    let descriptions = {};
+    let $this = $(this);
+    
+    $.get({
+      url: '../business-standards-api/grants/business-lifecycle/',
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+        for (let k in data) {
+          for (let i = 0, l = data[k].length; i < l; i++) {
+            let identifier = data[k][i]['Identifier'];
+            $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
+          }
+        }
+      }
+    });
+  });
 
-        $(document).ready(function () {
-          let descriptions = {};
-          let $this = $(this);
-          
-          $.get({
-            url: '../business-standards-api/grants/business-lifecycle/',
-            dataType: 'json',
-            success: function (data) {
-              console.log(data);
-              for (let k in data) {
-                for (let i = 0, l = data[k].length; i < l; i++) {
-                  let identifier = data[k][i]['Identifier'];
-                  $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
-                }
-              }
-            }
-          });
-        });
-
-      })(jQuery);
-      clearInterval(interval);
-    }
-  }, 100);
-})();
+})(jQuery);
