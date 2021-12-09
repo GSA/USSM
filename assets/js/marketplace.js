10182eb122a78f41e897479d494f7bec8a8f80bc
Matthew Petrovic <matt.t.petrovic@gmail.com>
2021-12-09 11:15:06 -0500

    Fix baseurl

--- assets/js/marketplace.js
+++ assets/js/marketplace.js
@@ -1,27 +1,29 @@
-let interval = setInterval(function () {
-  if (typeof jQuery != 'undefined') {
-    (function ($) {
+(function  () {
+  let interval = setInterval(function () {
+    if (typeof jQuery != 'undefined') {
+      (function ($) {
 
-      $(document).ready(function () {
-        let descriptions = {};
-        let $this = $(this);
-        
-        $.get({
-          url: '/business-standards-api/grants/business-lifecycle/',
-          dataType: 'json',
-          success: function (data) {
-            console.log(data);
-            for (let k in data) {
-              for (let i = 0, l = data[k].length; i < l; i++) {
-                let identifier = data[k][i]['Identifier'];
-                $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
+        $(document).ready(function () {
+          let descriptions = {};
+          let $this = $(this);
+          
+          $.get({
+            url: '/business-standards-api/grants/business-lifecycle/',
+            dataType: 'json',
+            success: function (data) {
+              console.log(data);
+              for (let k in data) {
+                for (let i = 0, l = data[k].length; i < l; i++) {
+                  let identifier = data[k][i]['Identifier'];
+                  $this.find('[data-identifier="' + identifier + '"]').siblings('.description-popup').html(data[k][i]['Description']);
+                }
               }
             }
-          }
+          });
         });
-      });
 
-    })(jQuery);
-    clearInterval(interval);
-  }
-}, 100);
+      })(jQuery);
+      clearInterval(interval);
+    }
+  }, 100);
+})();
