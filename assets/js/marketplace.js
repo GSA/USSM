45d297c0bcd1590cfd9fd7ca2b0a3b46597f8b84
Alex <alex.ntowe@gsa.gov>
2022-08-02 11:14:48 -0400

    feature/ECASOGP-6457
    feature/ECASOGP-6457 fixed the url path to be able to load

--- assets/js/marketplace.js
+++ assets/js/marketplace.js
@@ -2,9 +2,24 @@
   $(document).ready(function () {
     let descriptions = {};
     let $this = $(this);
-    
+
+    /**
+     * @type string
+     * @description to store the url prefix based on the environment url.cloudfront federalist url come with additional pathname. Using / as a prefix will removed these extra pathname and the url will be wrong
+     */
+    let prefixUrl = "/";
+
+    /**
+     * @type string
+     * @description used to capture the environment hostname
+     */
+    const hostname = window.location.hostname;
+    if(hostname.includes("federalist")) {
+      prefixUrl = "../../"; // because this url is used in the pathname:/marketplace/grm/ going to path back will send us to /
+    }
+
     $.get({
-      url: '/business-standards-api/grants/business-lifecycle/',
+      url: prefixUrl.concat('business-standards-api/grants/business-lifecycle/'),
       dataType: 'json',
       success: function (data) {
         console.log(data);
