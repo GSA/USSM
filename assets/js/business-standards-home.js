49bac60d7eeeb4dfc9ea96951a6180d084c2a625
Trigve Hagen <trigve.hagen@gsa.gov>
2022-09-15 07:56:33 -0700

    fixed alignment and added aria-labels to link.

--- assets/js/business-standards-home.js
+++ assets/js/business-standards-home.js
@@ -6,8 +6,10 @@ jQuery(document).ready(function ($) {
     e.preventDefault();
     if ($('.hr-management-services').css('display') == 'none') {
       $('#hr-management-services-main').html("Show Less <i class=\"hrms-arrow up\"></i>");
+      $('#hr-management-services-main').attr("aria-label", "Show More expanded");
     } else {
       $('#hr-management-services-main').html("Show More <i class=\"hrms-arrow down\"></i>");
+      $('#hr-management-services-main').attr("aria-label", "Show More collapsed");
     }
     jQuery(".hr-management-services").slideToggle();
   });
