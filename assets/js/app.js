acde9335d7b99e26f163bc9975e5f6d89d2f7160
Alex <alex.ntowe@gsa.gov>
2022-09-22 11:48:58 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -225,6 +225,6 @@ img1.click = function () {
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span.onclick = function () { 
     modal.style.display = "none";
 }
