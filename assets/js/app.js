0a74dc9db148ef02f9ea1ee94eb57da90c85214e
Alex <alex.ntowe@gsa.gov>
2022-09-26 10:05:20 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -176,6 +176,7 @@ function imageChecker(url){
                         else if (outerKey === "Business Use Cases" && UseCasesList.length > 0){
                             jQuery.each(outerValue, function (key1, value1) {
                                 let image = imageChecker(value1);
+                                console.log(value1); 
                                 let li = '<li><a href="'+value1+'">' + image + key1 + '</a></li>';
                                 jQuery("#" + page.UseCasesList ).append(li);
                             });
