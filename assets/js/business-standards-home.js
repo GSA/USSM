e5ac44572767314358964e8e52d3c250812648e6
HarikaMunipalle <harika.munipalle@gsa.gov>
2022-09-28 17:12:16 -0400

    Added plus icon

--- assets/js/business-standards-home.js
+++ assets/js/business-standards-home.js
@@ -5,10 +5,10 @@ jQuery(document).ready(function ($) {
   jQuery('#hr-management-services-main').click(function(e) {
     e.preventDefault();
     if ($('.hr-management-services').css('display') == 'none') {
-      $('#hr-management-services-main').html("Show Less <i class=\"hrms-arrow up\"></i>");
+        $('#hr-management-services-main').html("<i class=\"fas fa-minus-circle\"></i>");
       $('#hr-management-services-main').attr("aria-label", "Show Less expanded");
     } else {
-      $('#hr-management-services-main').html("Show More <i class=\"hrms-arrow down\"></i>");
+        $('#hr-management-services-main').html("<i class=\"fas fa-plus-circle\"></i>");
       $('#hr-management-services-main').attr("aria-label", "Show More collapsed");
     }
     jQuery(".hr-management-services").slideToggle();
